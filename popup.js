764b88b113c91886b8b7dcb509b8fc65092c0e6e
gskerry <gregory.skerry@gmail.com>
2017-04-10 20:17:26 -0400

    pull each array and display

--- popup.js
+++ popup.js
@@ -1,25 +1,28 @@
 
-function initstuff(){
-    chrome.storage.sync.set({'yays': [], 'nays':[]}, function(){
-        message('storage init.')
-    })
-}
-
-initstuff();
-
-// function dothestuff(){
-//
-//     chrome.storage.sync.get(null, function(data) {
-//         document.getElementById('yays').textContent = JSON.stringify(data);
-//         // message('Settings saved');
-//     });
-//
-//     chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-//         var tab = tabs[0];
-//         var url = tab.url;
-//         document.getElementById('theUrl').textContent = url;
-//         // chrome.storage.sync.set(, function(data) {
+// function initstuff(){
+//     chrome.storage.sync.set({'yays': [], 'nays':[]}, function(){
+//         message('storage init.')
 //     })
 // }
 //
-// dothestuff();
+// initstuff();
+
+function dothestuff(){
+
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        var tab = tabs[0];
+        var url = tab.url;
+        document.getElementById('theUrl').textContent = url;
+    })
+
+    chrome.storage.sync.get('yays', function(data) {
+        document.getElementById('yays').textContent = JSON.stringify(data.yays);
+    });
+
+    chrome.storage.sync.get('nays', function(data) {
+        document.getElementById('nays').textContent = JSON.stringify(data.nays);
+    });
+
+}
+
+dothestuff();
