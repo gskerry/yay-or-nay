a4ee492f8c7686e8c741705c246865bf218753e7
gskerry <gregory.skerry@gmail.com>
2017-04-10 20:04:11 -0400

    integrate chrome storage... return contents

--- popup.js
+++ popup.js
@@ -1,10 +1,16 @@
 
 function dothestuff(){
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-        var tab = tabs[0];
-        var url = tab.url;
-        document.getElementById('theUrl').textContent = url;
-    })
+
+    chrome.storage.sync.get(null, function(data) {
+        document.getElementById('theUrl').textContent = JSON.stringify(data);
+        // message('Settings saved');
+    });
+
+    // chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+    //     var tab = tabs[0];
+    //     var url = tab.url;
+    //     document.getElementById('theUrl').textContent = url;
+    // })
 }
 
 dothestuff();
