29ec01bacc99ea2f50e50e0b87f1e8f90b810ddb
gskerry <gregory.skerry@gmail.com>
2017-04-10 20:15:12 -0400

    init function to create the localStorage params

--- popup.js
+++ popup.js
@@ -1,16 +1,25 @@
 
-function dothestuff(){
-
-    chrome.storage.sync.get(null, function(data) {
-        document.getElementById('theUrl').textContent = JSON.stringify(data);
-        // message('Settings saved');
-    });
-
-    // chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-    //     var tab = tabs[0];
-    //     var url = tab.url;
-    //     document.getElementById('theUrl').textContent = url;
-    // })
+function initstuff(){
+    chrome.storage.sync.set({'yays': [], 'nays':[]}, function(){
+        message('storage init.')
+    })
 }
 
-dothestuff();
+initstuff();
+
+// function dothestuff(){
+//
+//     chrome.storage.sync.get(null, function(data) {
+//         document.getElementById('yays').textContent = JSON.stringify(data);
+//         // message('Settings saved');
+//     });
+//
+//     chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+//         var tab = tabs[0];
+//         var url = tab.url;
+//         document.getElementById('theUrl').textContent = url;
+//         // chrome.storage.sync.set(, function(data) {
+//     })
+// }
+//
+// dothestuff();
